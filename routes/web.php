c7dccf68ffa415cbacf711301acb1e9480ba34ca
NabilNugraha17 <nabilnugraha1717@gmail.com>
2025-01-30 23:18:40 +0800

    add create user + update controller+route

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create',[UserController::class,'create'])->name('users.create');
+    Route::post('/users/store',[UserController::class,'store'])->name('users.store');
+
 
     Route::get('/barang', [BarangController::class, 'index'])->name('barang.index');
 
